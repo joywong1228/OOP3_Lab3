5c4af4d0626db60faa41e8555b9b9e46d8fad910
joywong1228 <joywong1228@gmail.com>
2025-07-14 15:36:22 -0600

    update files

--- lab3StartingCode/src/implementations/Dictionary.java
+++ lab3StartingCode/src/implementations/Dictionary.java
@@ -1,16 +1,50 @@
 package implementations;
 
+import exceptions.DuplicateKeyException;
 import java.util.ArrayList;
-
 import utilities.DictionaryADT;
 
-public class Dictionary<K,V> implements DictionaryADT<K,V>
-{
-	// constant
+public class Dictionary<K, V> implements DictionaryADT<K, V> {
 	private static final int DEFAULT_SIZE = 10;
-	
-	// index indicates pairing, e.g. keys[1] is stored at values[1]
+
 	private ArrayList<K> keys;
 	private ArrayList<V> values;
 
+	public Dictionary() {
+		this(DEFAULT_SIZE);
+	}
+
+	public Dictionary(int size) {
+		this.keys = new ArrayList<>(size);
+		this.values = new ArrayList<>(size);
+	}
+
+	@Override
+	public void create(int size) {
+		// TODO: implement
+	}
+
+	@Override
+	public boolean insert(K key, V value) throws DuplicateKeyException {
+		// TODO: implement
+		return false;
+	}
+
+	@Override
+	public V remove(K key) {
+		// TODO: implement
+		return null;
+	}
+
+	@Override
+	public boolean update(K key, V value) {
+		// TODO: implement
+		return false;
+	}
+
+	@Override
+	public V lookup(K key) {
+		// TODO: implement
+		return null;
+	}
 }
